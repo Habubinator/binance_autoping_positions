c1b4d3459ff1e73bd3aaa00ac9aeac6476bb3251
Habubinator <61784574+Habubinator@users.noreply.github.com>
2024-03-12 09:51:41 +0200

    Minor changes

--- src/index.js
+++ src/index.js
@@ -11,16 +11,18 @@ const checkApi = async () => {
                 `https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]}`
             );
             if (newData.data) {
-                valueNew = newData.data.openInterest;
-                valueOld = objData["value-new"] || 1;
-                openInterest = (valueNew * 100) / valueOld - 100;
-                console.log(`${objData.pair} - ${openInterest.toFixed(2)}%`);
+                let valueNew = newData.data.openInterest;
+                let valueOld = objData["value-new"] || 1;
+                let openInterest = ((valueNew * 100) / valueOld - 100).toFixed(
+                    2
+                );
+                console.log(`${objData.pair} - ${openInterest}%`);
                 db.updateElement(
                     objData._id,
                     objData.pair,
                     valueOld,
                     valueNew,
-                    openInterest.toFixed(2)
+                    openInterest
                 );
             }
         } catch (error) {
@@ -35,9 +37,8 @@ const checkApi = async () => {
 
 const start = async () => {
     try {
-        checkApi().then(() =>
-            setTimeout(checkApi, process.env.TIMEOUT_IN_MINUTES * 60 * 1000)
-        );
+        await checkApi();
+        setInterval(checkApi, process.env.TIMEOUT_IN_MINUTES * 60 * 1000);
     } catch (error) {
         console.log(error);
     }
