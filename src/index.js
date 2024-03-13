22857e2c856e286ef3f9e45719a133767f396cf2
Habubinator <61784574+Habubinator@users.noreply.github.com>
2024-03-13 11:46:14 +0200

    SetInterval fix

--- src/index.js
+++ src/index.js
@@ -12,78 +12,81 @@ async function delay(ms) {
 }
 
 const checkApi = async () => {
-    let unixTime = await db.checkUNIX(unixStampLink);
-    if (unixTime) {
-        console.log(
-            `Script worked less than 30m ago. Waiting ${unixTime} seconds`
-        );
-        await delay(unixTime * 1000);
-    }
+    // Дуже костильний і дуже простий метод рішення проблеми з очікуванням інтервалу, працює, тому залишаю так.
+    while (true) {
+        let unixTime = await db.checkUNIX(unixStampLink);
+        if (unixTime) {
+            console.log(
+                `Script worked less than 30m ago. Waiting ${unixTime} seconds`
+            );
+            await delay(unixTime * 1000);
+        }
 
-    console.log(`Starting script`);
-    let allData = await db.getAll(cryptoLink);
-    if (allData && allData.length) {
-        for (const objData of allData) {
-            try {
-                let newData;
+        console.log(`Starting script`);
+        let allData = await db.getAll(cryptoLink);
+        if (allData && allData.length) {
+            for (const objData of allData) {
                 try {
-                    newData = await axios.get(
-                        `https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]}`
-                    );
-                } catch (error) {
-                    if (error.response && error.response.status === 418) {
-                        const retryAfter =
-                            error.response.headers["retry-after"];
-                        console.log(
-                            `https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]} Received status 418. \nWaiting ${retryAfter} seconds before retrying.`
-                        );
-                        await new Promise((resolve) =>
-                            setTimeout(resolve, retryAfter * 1000)
-                        );
+                    let newData;
+                    try {
                         newData = await axios.get(
                             `https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]}`
                         );
+                    } catch (error) {
+                        if (error.response && error.response.status === 418) {
+                            const retryAfter =
+                                error.response.headers["retry-after"];
+                            console.log(
+                                `https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]} Received status 418. \nWaiting ${retryAfter} seconds before retrying.`
+                            );
+                            await new Promise((resolve) =>
+                                setTimeout(resolve, retryAfter * 1000)
+                            );
+                            newData = await axios.get(
+                                `https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]}`
+                            );
+                        }
+                        if (error.response && error.response.status === 400) {
+                            console.log(
+                                `https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]} Received status 400.`
+                            );
+                            continue;
+                        } else {
+                            console.log(
+                                `For https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]} ` +
+                                    error.message
+                            );
+                        }
                     }
-                    if (error.response && error.response.status === 400) {
-                        console.log(
-                            `https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]} Received status 400.`
-                        );
-                        continue;
-                    } else {
-                        console.log(
-                            `For https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]} ` +
-                                error.message
-                        );
-                    }
-                }
 
-                if (newData && newData.data) {
-                    let valueNew = newData.data.openInterest;
-                    let valueOld = objData["value-new"] || 1;
-                    let openInterest = (
-                        (valueNew * 100) / valueOld -
-                        100
-                    ).toFixed(2);
-                    console.log(`${objData.pair} - ${openInterest}%`);
-                    db.updateElement(
-                        objData._id,
-                        objData.pair,
-                        valueOld,
-                        valueNew,
-                        openInterest
+                    if (newData && newData.data) {
+                        let valueNew = newData.data.openInterest;
+                        let valueOld = objData["value-new"] || 1;
+                        let openInterest = (
+                            (valueNew * 100) / valueOld -
+                            100
+                        ).toFixed(2);
+                        console.log(`${objData.pair} - ${openInterest}%`);
+                        db.updateElement(
+                            objData._id,
+                            objData.pair,
+                            valueOld,
+                            valueNew,
+                            openInterest
+                        );
+                    }
+                } catch (error) {
+                    console.log(
+                        `For https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]} ` +
+                            error.message
                     );
+                    continue;
                 }
-            } catch (error) {
-                console.log(
-                    `For https://fapi.binance.com/fapi/v1/openInterest?symbol=${objData["pair"]} ` +
-                        error.message
-                );
-                continue;
             }
+            console.log("Script succefully ended");
+        } else {
+            console.log("Bubble io database is offline or data set is empty");
         }
-        console.log("Script succefully ended");
-    } else {
-        console.log("Bubble io database is offline or data set is empty");
     }
 };
 
@@ -92,8 +95,7 @@ const start = async () => {
         if (timeOut) {
             clearTimeout(timeOut); // Очистити таймер, на випадок якщо все покращиться, щоб код не запускався двічі
         }
-        await checkApi();
-        setInterval(checkApi, process.env.TIMEOUT_IN_MINUTES * 60 * 1000);
+        checkApi();
     } catch (error) {
         console.log(error);
         timeOut = setTimeout(start, 5 * 60 * 1000); // Якщо бд лежить, наприклад, то повторити через 5 хвилин
